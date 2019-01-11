fcea6027ca3885b027adde5e47cb77414863e9e6
Ryan Holstad <ryan@motusglobal.com>
2019-01-11 12:45:57 -0500

    implement custom sendPasswordResetEmail for localization

--- src/index.js
+++ src/index.js
@@ -22,8 +22,39 @@ let SimpleSendGridAdapter = mailOptions => {
     });
   }
 
+  var sendPasswordResetEmail = function sendPasswordResetEmail(options) {
+    const user = options.user;
+    const to = user.get("email") || user.get('username');
+    const link = options.link;
+    const appName = options.appName;
+    const locale = options.locale;
+
+    var subject = "Password Reset Request for " + appName;
+    var text = "You requested to reset your password for " + appName +". Click here to reset it: " + link;
+
+    if (locale.includes("ja")) {
+      subject = "motusTHROWのパスワードリセット要求";
+      text = "motusTHROWのパスワードを再設定するよう要求しました。 リセットするにはここをクリックしてください：" + link;
+    }
+
+    return new Promise(function (resolve, reject) {
+      sendgrid.send({
+        from: mailOptions.fromAddress,
+        to: to,
+        subject: subject,
+        text: text
+      }, function(err, json) {
+        if (err) {
+          reject(err);
+        }
+        resolve(json);
+      });
+    });
+  };
+
   return Object.freeze({
-      sendMail: sendMail
+    sendMail: sendMail,
+    sendPasswordResetEmail: sendPasswordResetEmail
   });
 }
 
